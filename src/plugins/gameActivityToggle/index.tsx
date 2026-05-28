686257d120f794f2a9fd6ba43cf40de245b77f7b
RoScripter999 <orel.afriat2000@gmail.com>
2026-05-28 07:40:30 +0100

    Improve game activity toggle plugin

--- src/plugins/gameActivityToggle/index.tsx
+++ src/plugins/gameActivityToggle/index.tsx
@@ -16,11 +16,10 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import "./style.css";
-
 import { definePluginSettings } from "@api/Settings";
 import { getUserSettingLazy } from "@api/UserSettings";
 import ErrorBoundary from "@components/ErrorBoundary";
+import { Icon } from "@components/Icons";
 import { openPluginModal } from "@components/settings";
 import { Devs } from "@utils/constants";
 import { classes } from "@utils/misc";
@@ -33,7 +32,7 @@ import managedStyle from "./style.css?managed";
 // Same as StreamCrasher
 const Button = findComponentByCodeLazy(".GREEN,positionKeyStemOverride:");
 const Classes = findCssClassesLazy("audioButtonWithMenu", "audioButtonParent", "popoutOpen", "buttonChevron");
-const buttonClasses = findCssClassesLazy("redGlow", "enabled", "disabled", "plated");
+const ButtonClasses = findCssClassesLazy("redGlow", "enabled", "disabled", "plated");
 
 const ShowCurrentGame = getUserSettingLazy<boolean>("status", "showCurrentGame")!;
 
@@ -45,7 +44,7 @@ const settings = definePluginSettings({
     }
 });
 
-function makeIcon(showCurrentGame?: boolean, oldIcon?: boolean) {
+function ActivityToggleIcon(showCurrentGame?: boolean, oldIcon?: boolean) {
     const redLinePath = !oldIcon
         ? "M22.7 2.7a1 1 0 0 0-1.4-1.4l-20 20a1 1 0 1 0 1.4 1.4Z"
         : "M23 2.27 21.73 1 1 21.73 2.27 23 23 2.27Z";
@@ -54,30 +53,28 @@ function makeIcon(showCurrentGame?: boolean, oldIcon?: boolean) {
         ? "M23.27 4.73 19.27 .73 -.27 20.27 3.73 24.27Z"
         : "M23.27 4.54 19.46.73 .73 19.46 4.54 23.27 23.27 4.54Z";
 
-    return function () {
-        return (
-            <svg width="20" height="20" viewBox="0 0 24 24">
-                <path
-                    fill={!showCurrentGame && !oldIcon ? "var(--icon-voice-muted)" : "currentColor"}
-                    mask={!showCurrentGame ? "url(#gameActivityMask)" : void 0}
-                    d="M3.06 20.4q-1.53 0-2.37-1.065T.06 16.74l1.26-9q.27-1.8 1.605-2.97T6.06 3.6h11.88q1.8 0 3.135 1.17t1.605 2.97l1.26 9q.21 1.53-.63 2.595T20.94 20.4q-.63 0-1.17-.225T18.78 19.5l-2.7-2.7H7.92l-2.7 2.7q-.45.45-.99.675t-1.17.225Zm14.94-7.2q.51 0 .855-.345T19.2 12q0-.51-.345-.855T18 10.8q-.51 0-.855.345T16.8 12q0 .51.345 .855T18 13.2Zm-2.4-3.6q.51 0 .855-.345T16.8 8.4q0-.51-.345-.855T15.6 7.2q-.51 0-.855.345T14.4 8.4q0 .51.345 .855T15.6 9.6ZM6.9 13.2h1.8v-2.1h2.1v-1.8h-2.1v-2.1h-1.8v2.1h-2.1v1.8h2.1v2.1Z"
-                />
-                {!showCurrentGame && <>
-                    <path fill="var(--icon-voice-muted)" d={redLinePath} />
-                    <mask id="gameActivityMask">
-                        <rect fill="white" x="0" y="0" width="24" height="24" />
-                        <path fill="black" d={maskBlackPath} />
-                    </mask>
-                </>}
-            </svg>
-        );
-    };
+    return (
+        <svg width="20" height="20" viewBox="0 0 24 24">
+            <path
+                fill={!showCurrentGame && !oldIcon ? "var(--icon-voice-muted)" : "currentColor"}
+                mask={!showCurrentGame ? "url(#gameActivityMask)" : void 0}
+                d="M3.06 20.4q-1.53 0-2.37-1.065T.06 16.74l1.26-9q.27-1.8 1.605-2.97T6.06 3.6h11.88q1.8 0 3.135 1.17t1.605 2.97l1.26 9q.21 1.53-.63 2.595T20.94 20.4q-.63 0-1.17-.225T18.78 19.5l-2.7-2.7H7.92l-2.7 2.7q-.45.45-.99.675t-1.17.225Zm14.94-7.2q.51 0 .855-.345T19.2 12q0-.51-.345-.855T18 10.8q-.51 0-.855.345T16.8 12q0 .51.345 .855T18 13.2Zm-2.4-3.6q.51 0 .855-.345T16.8 8.4q0-.51-.345-.855T15.6 7.2q-.51 0-.855.345T14.4 8.4q0 .51.345 .855T15.6 9.6ZM6.9 13.2h1.8v-2.1h2.1v-1.8h-2.1v-2.1h-1.8v2.1h-2.1v1.8h2.1v2.1Z"
+            />
+            {!showCurrentGame && <>
+                <path fill="var(--icon-voice-muted)" d={redLinePath} />
+                <mask id="gameActivityMask">
+                    <rect fill="white" x="0" y="0" width="24" height="24" />
+                    <path fill="black" d={maskBlackPath} />
+                </mask>
+            </>}
+        </svg>
+    );
 }
 
 const ChevronIcon = ({ showCurrentGame, isShown }) => (
-    <svg width="15" height="15" fill="none" viewBox="0 0 24 24" style={{ transform: isShown ? "rotate(180deg)" : "none", transition: "transform 0.2s" }}>
+    <Icon width="15" height="15" style={{ transform: isShown ? "rotate(180deg)" : "none", transition: "transform 0.2s" }}>
         <path fill={!showCurrentGame ? "var(--icon-voice-muted)" : "currentColor"} d="M5.3 9.3a1 1 0 0 1 1.4 0l5.3 5.29 5.3-5.3a1 1 0 1 1 1.4 1.42l-6 6a1 1 0 0 1-1.4 0l-6-6a1 1 0 0 1 0-1.42Z" />
-    </svg>
+    </Icon>
 );
 
 function ActivityContextMenu({ closePopout }) {
@@ -96,6 +93,7 @@ function ActivityContextMenu({ closePopout }) {
                 id="game-activity-context-settings"
                 label="Activity Settings"
                 icon={Icons.SettingsIcon}
+                leadingAccessory={{ type: "icon", icon: Icons.SettingsIcon }}
                 action={() => openPluginModal(Velocity.Plugins.plugins.GameActivityToggle)}
             />
         </Menu.Menu>
@@ -114,26 +112,20 @@ function GameActivityToggleButton(props: { nameplate?: any; }) {
             animation={Popout.Animation.FADE}
             spacing={4}
             targetElementRef={buttonRef}
-            renderPopout={({ closePopout }) => (
-                <ActivityContextMenu closePopout={closePopout} />
-            )}
+            renderPopout={({ closePopout }) => <ActivityContextMenu closePopout={closePopout} />}
         >
             {({ onClick: openPopout }, { isShown }) => (
                 <div
                     ref={buttonRef}
-                    className={classes(Classes.audioButtonParent, !showCurrentGame && buttonClasses.redGlow, isShown && Classes.popoutOpen)}
+                    className={classes(Classes.audioButtonParent, !showCurrentGame && ButtonClasses.redGlow, isShown && Classes.popoutOpen)}
                 >
                     <Button
                         aria-checked={!showCurrentGame}
                         aria-label={showCurrentGame ? "Disable Game Activity" : "Enable Game Activity"}
                         className={Classes.audioButtonWithMenu}
-                        icon={makeIcon(showCurrentGame, oldIcon)}
-                        iconForeground={undefined}
-                        innerClassName={undefined}
+                        icon={ActivityToggleIcon(showCurrentGame, oldIcon)}
                         onClick={() => ShowCurrentGame.updateSetting(old => !old)}
                         onContextMenu={openPopout}
-                        onMouseEnter={undefined}
-                        onMouseLeave={undefined}
                         plated={props.nameplate != null}
                         redGlow={!showCurrentGame}
                         role="switch"
@@ -142,19 +134,14 @@ function GameActivityToggleButton(props: { nameplate?: any; }) {
                     />
                     <Button
                         aria-label="Activity Options"
-                        className={`${Classes.buttonChevron} ${isShown ? Classes.popoutOpen : ""}`}
+                        className={classes(Classes.buttonChevron, isShown && Classes.popoutOpen)}
                         icon={() => <ChevronIcon showCurrentGame={showCurrentGame} isShown={isShown} />}
                         onClick={openPopout}
                         onContextMenu={openPopout}
-                        onMouseEnter={undefined}
-                        onMouseLeave={undefined}
                         plated={props.nameplate != null}
                         redGlow={!showCurrentGame}
-                        tooltipForceOpen={false}
-                        tooltipPositionKey={undefined}
                         tooltipShouldShow={!isShown}
                         tooltipText="Activity Options"
-                        tooltipType={undefined}
                     />
                 </div>
             )}
@@ -172,16 +159,13 @@ export default definePlugin({
 
     managedStyle,
 
-    patches: [
-        {
-            find: ".DISPLAY_NAME_STYLES_COACHMARK)",
-            replacement: {
-                match: /children:\[(?=.{0,25}?accountContainerRef)/,
-                replace: "children:[$self.GameActivityToggleButton(arguments[0]),"
-            }
+    patches: [{
+        find: ".DISPLAY_NAME_STYLES_COACHMARK)",
+        replacement: {
+            match: /children:\[(?=.{0,25}?accountContainerRef)/,
+            replace: "children:[$self.GameActivityToggleButton(arguments[0]),"
         }
-    ],
+    }],
 
     GameActivityToggleButton: ErrorBoundary.wrap(GameActivityToggleButton, { noop: true })
-
 });
