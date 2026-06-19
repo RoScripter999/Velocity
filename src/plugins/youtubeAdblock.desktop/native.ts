4fed7eab14e72c8e0ae6885d238d6211617dd7be
RoScripter999 <orel.afriat2000@gmail.com>
2026-06-19 18:22:57 +0100

    Fix memory leak in youtubeAdBlock

--- src/plugins/youtubeAdblock.desktop/native.ts
+++ src/plugins/youtubeAdblock.desktop/native.ts
@@ -27,8 +27,6 @@ app.on("browser-window-created", (_, win) => {
 
             if (frame.url.includes("youtube.com/embed/")) {
                 frame.executeJavaScript(adguard);
-            } else if (frame.parent?.url.includes("youtube.com/embed/")) {
-                frame.parent.executeJavaScript(adguard);
             }
         });
     });
