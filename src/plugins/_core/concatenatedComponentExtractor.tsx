b4143d17f7305173efadea4997002b79c2cf18b7
RoScripter999 <orel.afriat2000@gmail.com>
2026-07-29 21:23:19 +0100

    useless fix

--- src/plugins/_core/concatenatedComponentExtractor.tsx
+++ src/plugins/_core/concatenatedComponentExtractor.tsx
@@ -51,11 +51,11 @@ export default definePlugin({
             }
         },
         {
-            find: ".currentTarget.files?.[0])",
-            lazy: true,
+            // Scrap this component one way or another.
+            find: '||"file-input",',
             replacement: {
-                match: /(?=function (\i)\(\i\)\{let\{filename:\i,className:\i,filters:\i,)/,
-                replace: "$self.setFilePicker($1);"
+                match: /class \i extends \i\.Component\{(?=_input)/,
+                replace: "$&static{$self.setFilePicker(this)};"
             }
         },
         {
